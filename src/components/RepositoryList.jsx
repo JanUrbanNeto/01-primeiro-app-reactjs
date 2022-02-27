471f23c0a6b6b61b9048d9212289c0d0c6e0da93
JanUrbanNeto <jan.urbanneto@gmail.com>
2022-02-27 19:50:40 -0300

    Mostrando lista dinâmica

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,23 +1,27 @@
+import { useEffect, useState } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
 
-const repository = {
-   name: "Jan Urban Neto",
-   description: "Forms in React",
-   link: "https://github.com/JanUrbanNeto"
-}
+// https://api.github.com/orgs/rocketseat/repos
 
 export function RepositoryList() {
+   const [respositories, setRepositories] = useState([])
+
+   useEffect(() => {
+      fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+   }, [])
+
    return (
       <section className="repository-list">
          <h1>Lista de Repositórios</h1>
 
          <ul>
-            <RepositoryItem repository={repository}/>
-            <RepositoryItem repository={repository}/>
-            <RepositoryItem repository={repository}/>
-            <RepositoryItem repository={repository}/>
+            {respositories.map(repository => {
+               return <RepositoryItem key={repository.name} repository={repository}/>
+            })}
          </ul>
       </section>
    )
